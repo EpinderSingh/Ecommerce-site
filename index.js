d62113217f43a5a2d4619c53d4c0c1e98089924f
Epinder <epinder.singh@gmail.com>
2020-06-26 12:49:26 +0800

    ecommerce project still in progress

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
@@ -7,10 +8,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(`
-    <div> 
-      <form method="POST"> 
+    <div>
+      <form method="POST">
         <input name="email" placeholder="email" />
-        <input name="paswword" placeholder="password" />
+        <input name="password" placeholder="password" />
         <input name="passwordConfirmation" placeholder="password confirmation" />
         <button>Sign Up</button>
       </form>
@@ -18,11 +19,21 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match');
+  }
+
   res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
-  console.log('Listening');
+  console.log('>>>>>>>>>Listening.........');
 });
