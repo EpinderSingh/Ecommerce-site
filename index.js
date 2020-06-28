ea2c29c2f0c7fde927afc8ff86ee62624c557c86
Epinder <epinder.singh@gmail.com>
2020-06-28 08:09:20 +0800

    Signup,Signin page created

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email in use');
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-
-  res.send('Account created!!!');
-});
+app.use(
+  cookieSession({
+    keys: ['f435t34rtf34ytg345tyg35rtg5r'],
+  }),
+);
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log('>>>>>>>>>Listening.........');
---
+++ routes/admin/auth.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+  res.send(signupTemplate({ req }));
+});
+
+router.post(
+  '/signup',
+  [
+    check('email')
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage('Must be  a valid email.'),
+    check('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters.'),
+    check('passwordConfirmation')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Must be between 4 and 20 characters.'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    const { email, password, passwordConfirmation } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email });
+    if (existingUser) {
+      return res.send('Email in use');
+    }
+
+    if (password !== passwordConfirmation) {
+      return res.send('Passwords must match');
+    }
+
+    // Create a user in our repo to represent this person
+    const user = await usersRepo.create({ email, password });
+
+    // Store the id of that user inside the users cokkie
+    req.session.userId = user.id;
+
+    res.send('Account created!!!');
+  },
+);
+
+router.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+  res.send(signinTemplate());
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not registered');
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password,
+  );
+  if (!validPassword) {
+    return res.send('Invalid password');
+  }
+
+  req.session.userId = user.id;
+  res.send('Your are signed in!!!');
+});
+
+module.exports = router;
